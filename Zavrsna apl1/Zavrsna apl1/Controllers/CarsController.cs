5c61d1126f61907a826cdbba95bfd0d418e2fedd
Banjac00 <banjacmarko2000@gmail.com>
2023-02-06 20:49:09 +0100

    Zavrsna aplikacija

---
+++ Zavrsna apl1/Zavrsna apl1/Controllers/CarsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Zavrsna_apl1.Models;
+
+namespace Zavrsna_apl1.Controllers
+{
+    public class CarsController : Controller
+    {
+        private CarEntities db = new CarEntities();
+
+        [Authorize]
+        // GET: Cars
+        public ActionResult Index()
+        {
+            if (User.IsInRole(RoleName.Admin) || User.IsInRole(RoleName.Employee))
+            {
+                var cars = db.Cars;
+                return View(cars.ToList());
+            }
+            return View("ReadOnlyList");
+        }
+
+        public ActionResult GetData()
+        {
+            var cars = db.Cars;
+            var data = cars.Select(el => new {
+                Id = el.ID,
+                Manufacturer = el.Manufacturer,
+                Model = el.Model,
+                LicensePlate = el.LicensePlate,
+                Year = el.Year,
+                Available = el.Available
+            });
+
+            return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        // GET: Cars/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // GET: Cars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cars/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Manufacturer,Model,LicensePlate,Year,Available")] Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cars.Add(car);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(car);
+        }
+
+        // GET: Cars/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // POST: Cars/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Manufacturer,Model,LicensePlate,Year,Available")] Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(car);
+        }
+
+        // GET: Cars/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // POST: Cars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Car car = db.Cars.Find(id);
+            db.Cars.Remove(car);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Zavrsna apl1/Zavrsna apl1/Models/RoleName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zavrsna_apl1.Models
+{
+    public class RoleName
+    {
+        public const string User = "User";
+
+        public const string Admin = "Admin";
+
+        public const string Employee = "Employee";
+    }
+}
